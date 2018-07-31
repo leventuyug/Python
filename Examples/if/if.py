3c36d46cf00e607d8ad9f0da5b56682897e47161
Levent UYUĞ <levent.jazz3@hotmail.com>
2018-07-31 21:38:02 +0300

    Add files via upload

---
+++ Examples/if/if.py
@@ -0,0 +1,13 @@
+# Get numbers
+
+num1 = input("Please write your first number. ")
+num2 = input("Please write your second number. ")
+
+# if blocks
+
+if num1 > num2:
+    print("Your first number is bigger than your second number.")
+elif num2 > num1:
+    print("Your second number is bigger than your first number.")
+else:
+    print("Both numbers are equal.")
